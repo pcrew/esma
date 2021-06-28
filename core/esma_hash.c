4307840b161af416a50985da1342b07b16e0dd88
Dmitry Lotakov <dlotakov@yandex.ru>
2021-06-28 15:29:18 +0300

    skeleton: continue

--- core/esma_hash.c
+++ core/esma_hash.c
@@ -4,7 +4,19 @@
 
 #include "esma_cpu.h"
 #include "esma_hash.h"
+#include "esma_alloc.h"
 #include "esma_logger.h"
+
+static u32 _hash_key(u8 *key, u32 len)
+{
+	u32 ret = 0;
+
+	for (int i = 0; i < len; i++) {
+		ret = ret * 32 + key[i];
+	}
+
+	return ret;
+}
 
 int esma_hashtable_init(struct esma_hashtable *table, char **keys, u32 nkeys)
 {
@@ -13,5 +25,18 @@ int esma_hashtable_init(struct esma_hashtable *table, char **keys, u32 nkeys)
 
 struct esma_hashtable *esma_hashtable_new(char **keys, u32 nkeys)
 {
-	return NULL;
+	struct esma_hashtable *table = esma_malloc(sizeof(struct esma_hashtable));
+	int err;
+
+	if (NULL == table) {
+		return NULL;
+	}
+
+	err = esma_hashtable_init(table, keys, nkeys);
+	if (err) {
+		esma_free(table);
+		return NULL;
+	}
+
+	return table;
 }
