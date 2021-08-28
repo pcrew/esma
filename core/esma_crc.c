3c0ad35b2c5529972655d294d754d39b4d14525b
Dmitry Lotakov <dlotakov@yandex.ru>
2021-08-28 17:09:56 +0300

    core/esma_crc fix error and typos

--- core/esma_crc.c
+++ core/esma_crc.c
@@ -4,6 +4,11 @@
 
 #include "esma_crc.h"
 
+struct crc8_poly {
+	u8 normal;
+	u8 revers;
+};
+
 struct crc16_poly {
 	u16 normal;
 	u16 revers;
@@ -14,27 +19,109 @@ struct crc32_poly {
 	u32 revers;
 };
 
-
-static void __crc16_false_false(u8 *d, u32 len, u16 crc, u16 poly)
+static u8 __crc8_false_false(u8 *d, u32 len, u8 crc, u8 poly)
 {
-	u8 i;
-
-	while (len--) {	
-		crc ^= *d++ << 8;
-		for (i = 0; i < 8; i++)
-			crc = crc & 0x8000 ? (crc << 1) ^ poly : crc << 1;
-	}
-}
-
-static void __crc16_true_true(u8 *d, u32 len, u16 crc, u16 poly)
-{
-	u8 i;
-
 	while (len--) {
 		crc ^= *d++;
-		for (i = 0; i < 8; i++)
+		for (u8 i = 0; i < 8; i++)
+			crc = crc & 0x80 ? (crc << 1) ^ poly : crc << 1;
+	}
+
+	return crc;
+}
+
+static u8 __crc8_true_true(u8 *d, u32 len, u8 crc, u8 poly)
+{
+	while (len--) {
+		crc ^= *d++;
+		for (u8 i = 0; i < 8; i++)
 			crc = crc & 1 ? (crc >> 1) ^ poly : crc >> 1;
 	}
+
+	return crc;
+}
+
+static struct crc8_poly crc8_poly[] = {
+	[0] = {
+		.normal = 0x07,
+		.revers = 0xD0,
+	},
+	[1] = {
+		.normal = 0x39, //0011 1001 / 1001 1100
+		.revers = 0x9C,
+	},
+	[2] = {
+		.normal = 0x31, //0011 0001 / 1000 1100
+		.revers = 0x8C,
+	},
+	[3] = {
+		.normal = 0x9B, //1001 1011 / 1101 1001
+		.revers = 0xBD,
+	},
+	[4] = {
+		.normal = 0x07,
+		.revers = 0xD0,
+	}
+};
+
+int esma_crc8(u8 *d, u32 len, u8 *crc, u32 name)
+{
+
+	u8 init;
+
+	switch (name) {
+	case CRC8_BASIC:
+		init = 0x00;
+		*crc = __crc8_false_false(d, len, init, crc8_poly[0].normal);
+		break;
+
+	case CRC8_CDMA2000:
+		init = 0xFF;
+		*crc = __crc8_false_false(d, len, init, crc8_poly[3].normal);
+		break;
+
+	case CRC8_DARC:
+		init = 0x00;
+		*crc = __crc8_true_true(d, len, init, crc8_poly[1].revers);
+		break;
+
+	case CRC8_WCDMA:
+		init = 0x00;
+		*crc = __crc8_true_true(d, len, init, crc8_poly[3].revers);
+		break;
+
+	case CRC8_MAXIM:
+		init = 0xFF;
+		*crc = __crc8_true_true(d, len, init, crc8_poly[2].revers);
+		break;
+
+	default:
+		return 1;
+	}
+
+	return 0;
+}
+
+static u16 __crc16_false_false(u8 *d, u32 len, u16 crc, u16 poly)
+{
+	while (len--) {	
+		crc ^= *d++ << 8;
+		for (u8 i = 0; i < 8; i++)
+			crc = crc & 0x8000 ? (crc << 1) ^ poly : crc << 1;
+	}
+
+	return crc;
+}
+
+static u16 __crc16_true_true(u8 *d, u32 len, u16 crc, u16 poly)
+{
+	while (len--) {
+		crc ^= *d++;
+		for (u8 i = 0; i < 8; i++)
+			crc = crc & 1 ? (crc >> 1) ^ poly : crc >> 1;
+	}
+
+	return crc;
 }
 
 static struct crc16_poly crc16_poly[] = {
@@ -68,157 +155,144 @@ static struct crc16_poly crc16_poly[] = {
 	},
 };
 
-u16 esma_crc16(u8 *d, u32 len, u32 name)
+int esma_crc16(u8 *d, u32 len, u16 *crc, u32 name)
 {
-	u16 crc;
+	u16 init;
 	switch (name) {
+	case CRC16_ARC:
+		init = 0x0000;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[1].revers);
+		break;
 
-		case ARC:
-			crc = 0x0000;
-			__crc16_true_true(d, len, crc, crc16_poly[1].revers);
-			return crc;
+	case CRC16_AUG_CCITT:
+		init = 0x1D0F;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[0].normal);
+		break;
 
-		case AUG_CCITT:
-			crc = 0x1D0F;
-			__crc16_false_false(d, len, crc, crc16_poly[0].normal);
-			return crc;
+	case CRC16_BUYPASS:
+		init = 0x0000;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[1].normal);
+		break;
 
-		case BUYPASS:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[1].normal);
-			return crc;
+	case CRC16_CDMA2000:
+		init = 0xFFFF;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[2].normal);
+		break;
 
-		case CDMA2000:
-			crc = 0xFFFF;
-			__crc16_false_false(d, len, crc, crc16_poly[2].normal);
-			return crc;
+	case CRC16_DDS_110:
+		init = 0x800D;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[1].normal);
+		break;
 
-		case DDS_110:
-			crc = 0x800D;
-			__crc16_false_false(d, len, crc, crc16_poly[1].normal);
-			return crc;
+	case CRC16_DECT_R:
+		init = 0x0000;
+		*crc = 0x0001 ^ __crc16_false_false(d, len, init, crc16_poly[3].normal);
+		break;
 
-		case DECT_R:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[3].normal);
-			return crc ^ 0x0001;
+	case CRC16_DECT_X:
+		init = 0x0000;
+		*crc =  __crc16_false_false(d, len, init, crc16_poly[3].normal);
+		break;
 
-		case DECT_X:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[3].normal);
-			return crc;
+	case CRC16_DNP:
+		init = 0x0000;
+		*crc = ~__crc16_true_true(d, len, init, crc16_poly[4].revers);
+		break;
 
-		case DNP:
-			crc = 0x0000;
-			__crc16_true_true(d, len, crc, crc16_poly[4].revers);
-			return ~crc;
+	case CRC16_EN_13757:
+		init = 0x0000;
+		*crc = ~__crc16_false_false(d, len, init, crc16_poly[4].normal);
+		break;
 
-		case EN_13757:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[4].normal);
-			return ~crc;
+	case CRC16_GENIBUS:
+		init = 0xFFFF;
+		*crc =  ~__crc16_false_false(d, len, init, crc16_poly[0].normal);
+		break;
 
-		case GENIBUS:
-			crc = 0xFFFF;
-			__crc16_false_false(d, len, crc, crc16_poly[0].normal);
-			return ~crc;
+	case CRC16_MAXIM:
+		init = 0x0000;
+		*crc = ~__crc16_true_true(d, len, init, crc16_poly[1].revers);
+		break;
 
-		case MAXIM:
+	case CRC16_MCRF4XX:
+		init = 0xFFFF;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
+	case CRC16_RIELLO:
+		init = 0x554D;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
 
-			crc = 0x0000;
-			__crc16_true_true(d, len, crc, crc16_poly[1].revers);
-			return ~crc;
+	case CRC16_T10_DIF:
+		init = 0x0000;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[5].normal);
+		break;
 
-		case MCRF4XX:
-			crc = 0xFFFF;
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return crc;
+	case CRC16_TELEDISK:
+		init = 0x0000;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[6].normal);
+		break;
 
-		case RIELLO:
-			/*  B    2    A    A
-			 * 1011 0010 1010 1010 - normal
-			 * 0101 0101 0100 1101 - reversed
-			 *  5    5    4    D
-			 * */
-			crc = 0x554D;
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return crc;
+	case CRC16_TMS37157:
+		init = 0x3791;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
 
-		case T10_DIF:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[5].normal);
-			return crc;
+	case CRC16_USB:
+		init = 0xFFFF;
+		*crc = ~__crc16_true_true(d, len, init, crc16_poly[1].revers);
+		break;
+	case CRC16_A:
+		init = 0x6363;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
 
-		case TELEDISK:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[6].normal);
-			return crc;
+	case CRC16_KERMIT:
+		init = 0x0000;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
 
-		case TMS37157:
-			crc = 0x3791; /* reversed 0x89EX */
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return crc;
+	case CRC16_MODBUS:
+		init = 0xFFFF;
+		*crc = __crc16_true_true(d, len, init, crc16_poly[1].revers);
+		break;
 
-		case USB:
-			crc = 0xFFFF;
-			__crc16_true_true(d, len, crc, crc16_poly[1].revers);
-			return ~crc;
+	case CRC16_X_25:
+		init = 0xFFFF;
+		*crc = ~__crc16_true_true(d, len, init, crc16_poly[0].revers);
+		break;
 
-		case A:
-			/*  C    6    C    6
-			 * 1100 0110 1100 0110 - normal
-			 * 0110 0011 0110 0011 - reversed
-			 *  6    3    6    3
-			 *  */
-			crc = 0x6363;
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return crc;
-
-		case KERMIT:
-			crc = 0x0000;
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return crc;
-
-		case MODBUS:
-			crc = 0xFFFF;
-			__crc16_true_true(d, len, crc, crc16_poly[1].revers);
-			return crc;
-
-		case X_25:
-			crc = 0xFFFF;
-			__crc16_true_true(d, len, crc, crc16_poly[0].revers);
-			return ~crc;
-
-		case XMODEM:
-			crc = 0x0000;
-			__crc16_false_false(d, len, crc, crc16_poly[0].normal);
-			return crc;
-
-		default:
-			return 0;
+	case CRC16_XMODEM:
+		init = 0x0000;
+		*crc = __crc16_false_false(d, len, init, crc16_poly[0].normal);
+		break;
+	default:
+		return 1;
 	}
+
+	return 0;
 }
 
-static void __crc32_false_false(u8 *d, u32 len, u32 crc, u32 poly)
+static u32 __crc32_false_false(u8 *d, u32 len, u32 crc, u32 poly)
 {
-	u8 i;
-
 	while (len--) {
 		crc ^= *d++ << 24;
-		for (i = 0; i < 8; i++)
+		for (u8 i = 0; i < 8; i++)
 			crc = crc & 0x80000000 ? (crc << 1) ^ poly : crc << 1;
 	}
+
+	return crc;
 }
 
-static void __crc32_true_true(u8 *d, u32 len, u32 crc, u32 poly)
+static u32 __crc32_true_true(u8 *d, u32 len, u32 crc, u32 poly)
 {
-	u8 i;
-
 	while (len--) {
 		crc ^= *d++;
-		for (i = 0; i < 8; i++)
+		for (u8 i = 0; i < 8; i++)
 			crc = crc & 1 ? (crc >> 1) ^ poly : crc >> 1;
 	}
+
+	return crc;
 }
 
 /*  1    E    D    C    6    F    4    1
@@ -262,58 +336,59 @@ static struct crc32_poly crc32_poly[] = {
 	},
 };
 
-u32 esma_crc32(u8 *d, u32 len, u32 name)
+int esma_crc32(u8 *d, u32 len, u32 *crc, u32 name)
 {
-	u32 crc;
+	u32 init;
 
 	switch (name) {
+	case CRC32_BASIC:
+		init = 0xFFFFFFFF;
+		*crc = ~__crc32_true_true(d, len, init, crc32_poly[0].revers);
+		break;
 
-		case BASIC:
-			crc = 0xFFFFFFFF;
-			__crc32_true_true(d, len, crc, crc32_poly[0].revers);
-			return ~crc;
+	case CRC32_BZIP2:
+		init = 0xFFFFFFFF;
+		*crc = ~__crc32_false_false(d, len, init, crc32_poly[0].normal);
+		break;
 
-		case BZIP2:
-			crc = 0xFFFFFFFF;
-			__crc32_false_false(d, len, crc, crc32_poly[0].normal);
-			return ~crc;
+	case CRC32_C:
+		init = 0xFFFFFFFF;
+		*crc = ~__crc32_true_true(d, len, init, crc32_poly[1].revers);
+		break;
 
-		case C:
-			crc = 0xFFFFFFFF;
-			__crc32_true_true(d, len, crc, crc32_poly[1].revers);
-			return ~crc;
+	case CRC32_D:
+		init = 0xFFFFFFFF;
+		*crc = ~__crc32_true_true(d, len, init, crc32_poly[2].revers);
+		break;
 
-		case D:
-			crc = 0xFFFFFFFF;
-			__crc32_true_true(d, len, crc, crc32_poly[2].revers);
-			return ~crc;
+	case CRC32_MPEG2:
+		init = 0xFFFFFFFF;
+		*crc = __crc32_false_false(d, len, init, crc32_poly[0].normal);
+		break;
 
-		case MPEG2:
-			crc = 0xFFFFFFFF;
-			__crc32_false_false(d, len, crc, crc32_poly[0].normal);
-			return crc;
+	case CRC32_POSIX:
+		init = 0x00000000;
+		*crc = ~__crc32_false_false(d, len, init, crc32_poly[0].normal);
+		break;
 
-		case POSIX:
-			crc = 0x00000000;
-			__crc32_false_false(d, len, crc, crc32_poly[0].normal);
-			return ~crc;
+	case CRC32_Q:
+		init = 0x00000000;
+		*crc = __crc32_false_false(d, len, init, crc32_poly[3].normal);
+		break;
 
-		case Q:
-			crc = 0x00000000;
-			__crc32_false_false(d, len, crc, crc32_poly[3].normal);
-			return crc;
+	case CRC32_JAMCRC:
+		init = 0xFFFFFFFF;
+		*crc = __crc32_true_true(d, len, init, crc32_poly[0].revers);
+		break;
 
-		case JAMCRC:
-			crc = 0xFFFFFFFF;
-			__crc32_true_true(d, len, crc, crc32_poly[0].revers);
-			return crc;
+	case CRC32_XFER:
+		init = 0x00000000;
+		*crc = __crc32_false_false(d, len, init, crc32_poly[4].normal);
+		break;
 
-		case XFER:
-			crc = 0x00000000;
-			__crc32_false_false(d, len, crc, crc32_poly[4].normal);
-			return crc;
-
-		default:
-			return 0;
+	default:
+		return 1;
 	}
+
+	return 0;
 }
--- core/esma_crc.h
+++ core/esma_crc.h
@@ -5,55 +5,49 @@
 #include "common/numeric_types.h"
 
 /* CRC 8 */
-#define BASIC		0x00
-#define AUTOSTAR	0x01
-#define BLUETOOTH	0x02
-#define CCITT		0x03
-#define DALLAS		0x04
-#define MAXIM		DALLAS
-#define DARC		0x06
-#define GSM_B		0x07
-#define SAE_J1850	0x08
-#define WCDMA		0x09
+#define CRC8_BASIC	0x00
+#define CRC8_CDMA2000	0x01
+#define CRC8_DARC	0x02
+#define CRC8_WCDMA	0x03
+#define CRC8_MAXIM	0x04
 
 /* CRC 16 */
-#define ARC		0x0A
-#define AUG_CCITT	0x0B
-#define BUYPASS		0x0C
-#define CCITT_FALE	0x0D
-#define CDMA2000	0x0E
-#define DDS_110		0x0F
-#define DECT_R		0x10
-#define DECT_X		0x11
-#define DNP		0x12
-#define EN_13757	0x13
-#define GENIBUS		0x14
-#define MAXIM		DALLAS
-#define MCRF4XX		0x16
-#define RIELLO		0x17
-#define T10_DIF		0x18
-#define TELEDISK	0x19
-#define TMS37157	0x1A
-#define USB		0x1B
-#define A		0x1C
-#define KERMIT		0x1D
-#define MODBUS		0x1E
-#define X_25		0x1F
-#define XMODEM		0x20
+#define CRC16_ARC	0x00
+#define CRC16_AUG_CCITT	0x01
+#define CRC16_BUYPASS	0x02
+#define CRC16_CDMA2000	0x03
+#define CRC16_DDS_110	0x04
+#define CRC16_DECT_R	0x05
+#define CRC16_DECT_X	0x06
+#define CRC16_DNP	0x07
+#define CRC16_EN_13757	0x08
+#define CRC16_GENIBUS	0x09
+#define CRC16_MAXIM	0x0a
+#define CRC16_MCRF4XX	0x0b
+#define CRC16_RIELLO	0x0c
+#define CRC16_T10_DIF	0x0d
+#define CRC16_TELEDISK	0x0e
+#define CRC16_TMS37157	0x0f
+#define CRC16_USB	0x10
+#define CRC16_A		0x11
+#define CRC16_KERMIT	0x12
+#define CRC16_MODBUS	0x13
+#define CRC16_X_25	0x14
+#define CRC16_XMODEM	0x15
 
 /* CRC 32 */
-#define BZIP2		0x22
-#define C		0x23
-#define D		0x24
-#define MPEG2		0x25
-#define POSIX		0x26
-#define Q		0x27
-#define JAMCRC		0x28
-#define XFER		0x29
+#define CRC32_BASIC	0x00
+#define CRC32_BZIP2	0x01
+#define CRC32_C		0x02
+#define CRC32_D		0x03
+#define CRC32_MPEG2	0x04
+#define CRC32_POSIX	0x05
+#define CRC32_Q		0x06
+#define CRC32_JAMCRC	0x07
+#define CRC32_XFER	0x08
 
-u8 esma_crc8(u8 *d, u32 len, u32 name);
-u16 esma_crc16(u8 *d, u32 len, u32 name);
-u32 esma_crc32(u8 *d, u32 len, u32 name);
-u64 esma_crc64(u8 *d, u32 len, u32 name);
+int esma_crc8(u8 *d, u32 len, u8 *crc, u32 name);
+int esma_crc16(u8 *d, u32 len, u16 *crc, u32 name);
+int esma_crc32(u8 *d, u32 len, u32 *crc, u32 name);
 
 #endif
